4decd4f079c9c67aa4276bd53ba4e89e9bef380a
Irene <irene.martinezcarrasco@usp.ceu.es>
2025-04-24 13:13:48 +0200

    resta en el main

--- mainPractica.py
+++ mainPractica.py
@@ -1,4 +1,5 @@
 import maria as M
+import irene as I 
 
 def main():
     a = 1
@@ -6,5 +7,8 @@ def main():
     c = M.sumaA(a,b)
     print(f"El resultado de la suma de a y b es {c}")
 
+    d = I.resta(a, b)
+    print(f"El valor de la resta de a y b es: {d}")
+
 if __name__ == "__main__":
-    main()
+    main() 
